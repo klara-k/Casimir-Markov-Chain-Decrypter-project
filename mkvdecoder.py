a3c634a73f7915833af7ecb8df6425a5aad7af4b
Zhenya Cheipesh <gene.cheypesh@gmail.com>
2018-11-08 15:57:25 +0200

    Zhenya added text_to_matrix

--- mkvdecoder.py
+++ mkvdecoder.py
@@ -3,7 +3,17 @@ import numpy as np
 
 # zhenya
 def text_to_matrix(text, is_normalised):
-	pass
+    M = np.zeros((26,26))
+    text_lower = text.lower()
+    N = 0
+    for i in range(len(text_lower)-1):
+        if text_lower[i] != ' ' and text_lower[i+1] != ' ':
+            N += 1
+            M[ord(text_lower[i])-97,ord(text_lower[i+1])-97] += 1
+    if is_normalised:
+        return M/N
+    else:
+        return M
 
 # klara
 def decode_text(text, cipher):
