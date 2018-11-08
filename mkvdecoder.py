e30c4e64d61312a5575fe9a207f0cecd64ba96c3
Stefano <onafets94@yahoo.it>
2018-11-08 13:59:46 +0100

    Job subdivision for main module

---
+++ mkvdecoder.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+# zhenya
+def text_to_matrix(text, is_normalised):
+	pass
+
+# klara
+def decode_text(text, cipher):
+	pass
+
+# stefano
+def decode_matrix(encoded_matrix, cipher):
+	pass
+
+# yaroslav
+def fitness(ref_matrix, guess_matrix):
+	pass
+
+# gal
+def metropolis_step(ref_matrix, enc_matrix, cipher):
+	pass
