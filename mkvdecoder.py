e9d457b0752cde91fb797cd0679b32d314478a07
tarrlikh <yaroslav.gerasimenko@gmail.com>
2018-11-09 14:59:43 +0100

    logfitness function added

--- mkvdecoder.py
+++ mkvdecoder.py
@@ -146,6 +146,23 @@ def fitness(ref_matrix, guess_matrix):
 #     return(np.exp(np.trace(np.transpose(guess_matrix) .dot (np.log(ref_matrix) ) ) ))
     return(np.exp(logf))
 
+def logfitness(ref_matrix, guess_matrix):
+    """Returns the value of the fitness function.
+    
+    The input is the reference matrix and the guess matrix. The reference matrix is assumed to be regularized (to contain no zero elements)"""
+    
+    logf=0.
+    
+    for i in range (len(ref_matrix)):
+        for j in range (len(ref_matrix)):
+            logf=logf+(np.log(ref_matrix[i,j])*int(guess_matrix[i,j]))
+    
+    # Convertion from the product to an exponential of a sum is being used
+    # transpose is needed to compute M_ij log(N_ij) and not M_ij log(N_ji)
+    
+#     return(np.exp(np.trace(np.transpose(guess_matrix) .dot (np.log(ref_matrix) ) ) ))
+    return(logf)
+
 def fitness_ratio(ref_matrix, guess_matrix_old, guess_matrix_new):
     """Returns the ratio of the old and the new fitness functions (old over new).
     
